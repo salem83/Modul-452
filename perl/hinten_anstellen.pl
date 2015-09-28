471106fcffb990c5cfe46abbc214662e92ed4a22
Boris Däppen <boris_daeppen@bluewin.ch>
2015-09-28 15:43:20 +0200

    IO schöner formatiert (web)

--- perl/hinten_anstellen.pl
+++ perl/hinten_anstellen.pl
@@ -72,15 +72,19 @@ __DATA__
 
 @@ anstellen.html.ep
 <!DOCTYPE html>
-<html lang="de"><head><meta charset="utf-8"><title>Anstellen</title></head>
+<html lang="de"><head>
+ <meta charset="utf-8">
+ <title>Aufrufen</title>
+ <style> table, td, th { border: 0px solid black; } </style>
+</head>
 <body>
 <h1>Anstellen</h1>
 <form action="/anstellen">
-<p>
- <input type="text" name="vorname" value="">
- <input type="text" name="nachname" value="">
- <input type="text" name="alter" value="">
-</p>
+<table>
+<tr><td>Vorname:</td><td><input type="text" name="vorname" value=""></td></tr>
+<tr><td>Nachname:</td><td><input type="text" name="nachname" value=""></td></tr>
+<tr><td>Alter:</td><td><input type="text" name="alter" value=""></td></tr>
+</table>
 <input type="submit" value="Absenden">
 </form> 
 </body>
@@ -89,17 +93,21 @@ __DATA__
 
 @@ aufrufen.html.ep
 <!DOCTYPE html>
-<html lang="de"><head><meta charset="utf-8"><title>Aufrufen</title></head>
+<html lang="de"><head>
+ <meta charset="utf-8">
+ <title>Aufrufen</title>
+ <style> table, td, th { border: 0px solid black; } </style>
+</head>
 <body>
 <h1>Aufrufen</h1>
 <form action="/aufrufen">
 <input type="submit" name="aufruf"  value="Person aufrufen">
 </form> 
 <h2>Nächste Person</h2>
-<table border=0>
-<tr><td>Vorname</td><td><%= $vorname %></td></tr>
-<tr><td>Nachname</td><td><%= $nachname %></td></tr>
-<tr><td>Alter</td><td><%= $alter %></td></tr>
+<table>
+<tr><td>Vorname:</td><td><%= $vorname %></td></tr>
+<tr><td>Nachname:</td><td><%= $nachname %></td></tr>
+<tr><td>Alter:</td><td><%= $alter %></td></tr>
 </table>
 Anzahl Personen in der Schlange: <%= $laenge %>
 </body>
